5904f4b9f502a24e5e9c0365de81319f77a39320
cv <zhiming.chen@yunlsp.com>
2018-12-10 01:01:12 +0800

    C.v.

--- source_code/primes_in_numbers/Primes_in_numbers_Cv.java
+++ source_code/primes_in_numbers/Primes_in_numbers_Cv.java
@@ -1,9 +1,5 @@
 package primes_in_numbers;
 
-
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 /**
  * @description: 数字中的质因数
  * @author: C.v.
@@ -11,49 +7,48 @@ import java.util.Map;
  */
 public class Primes_in_numbers_Cv {
 
-    /**
-     * 记录质因数出现的次数，对应key(质因数)-value(次数)
-     */
-    static Map<Integer, Integer> primes = new LinkedHashMap<>();
 
-    /**
-     * 添加质因数
-     * @param i 质因数
-     */
-    static void addPrimes(int i) {
-        primes.put(i, primes.get(i) == null ? 1 : primes.get(i)+1);
+    public static String factors(int n) {
+        StringBuffer sb = new StringBuffer();
+        rho(n, sb);
+        addPrimes(0, sb);
+        return sb.toString();
     }
 
     /**
      * Pollard Rho因数分解
      * @param n 待分解的数
      */
-    static void rho(int n) {
+    static void rho(int n, StringBuffer sb) {
         if (n < 2) return;
         for (int i=2; i<=n; i++) {
             while (n != i) {
                 if (n%i != 0) break;//不能整除肯定不是因数
-                addPrimes(i);
+                addPrimes(i, sb);
                 n = n/i;
             }
         }
-        addPrimes(n);
+        addPrimes(n, sb);
     }
 
     /**
      * 输出格式处理
      */
-    static String format() {
-        StringBuffer sbuffer = new StringBuffer();
-        primes.forEach((k,v) ->
-                sbuffer.append(v == 1 ? "(" + k+ ")" : "(" + k + "**" + v + ")"));
-        return sbuffer.toString();
+    static int lastPrimes, count;
+
+    static void addPrimes(int k, StringBuffer sb) {
+        if (lastPrimes == k) {
+            count++;
+            return;
+        }
+        if (lastPrimes != 0) sb.append(count == 1 ? "(" + lastPrimes+ ")" : "(" + lastPrimes + "**" + count + ")");
+
+        lastPrimes = k;
+        count = 1;
     }
 
     public static void main (String[] args) {
-        int n = 6105420;
-        rho(n);
-        System.out.println(format());
+        int n = 7775460;
+        System.out.println(factors(n));
     }
-
 }
