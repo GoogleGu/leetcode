8aebec1f50512b4b0ffa67fce7fb6a9c6863a3f7
arthur_gu <gyx152540@163.com>
2018-12-12 13:48:51 +0800

    上传了ant bridge的答案

--- source_code/ant_bridge/ant_bridge_arthur.py
+++ source_code/ant_bridge/ant_bridge_arthur.py
@@ -5,6 +5,19 @@ from queue import deque
 
 
 def ant_bridge(ants, terrain):
+
+    def march_forward(marching_ants, bridge_ants):
+        """
+        将整个行进蚁前移一格，并拆掉最尾端空闲的桥蚁
+        :param marching_ants: 行进蚁队列
+        :param bridge_ants: 桥蚁队列
+        """
+        print(marching_ants, bridge_ants)
+        for ant in marching_ants:
+            ant[1] += 1
+        if len(marching_ants) == 0 or (len(bridge_ants) > 0 and bridge_ants[0][1] < marching_ants[0][1]):
+            marching_ants.appendleft(bridge_ants.popleft())
+
     terrains = '-' + terrain + '-'
     marching_ants = deque([ant, -len(ants)+count+1] for count, ant in enumerate(ants))
     print(marching_ants)
@@ -26,17 +39,13 @@ def ant_bridge(ants, terrain):
     return ''.join(ant[0] for ant in marching_ants)
 
 
-def march_forward(marching_ants, bridge_ants):
-    """
-    将整个行进蚁前移一格，并拆掉最尾端空闲的桥蚁
-    :param marching_ants: 行进蚁队列
-    :param bridge_ants: 桥蚁队列
-    """
-    print(marching_ants, bridge_ants)
-    for ant in marching_ants:
-        ant[1] += 1
-    if len(marching_ants) == 0 or (len(bridge_ants) > 0 and bridge_ants[0][1] < marching_ants[0][1]):
-        marching_ants.appendleft(bridge_ants.popleft())
+def best_practice(ants, terrains):
+    n_pits = 0
+    for pos in range(1, len(terrains) - 1):
+        if '.' in terrains[pos-1:pos+2]:
+            n_pits += 1
+    n_pits = n_pits % len(ants)
+    return  ants[-n_pits:] + ants[:-n_pits]
 
 
 if __name__ == '__main__':
