1968fe4c89c94d8c293e5b54d57ffc0796a0ace1
arthur_gu <gyx152540@163.com>
2018-12-23 23:13:48 +0800

    上传了答案：integers recreation one

--- source_code/integers_recreation_one/integers_recreation_one_arthur.py
+++ source_code/integers_recreation_one/integers_recreation_one_arthur.py
@@ -16,11 +16,9 @@ def list_squared(m, n):
 
 def sum_of_factors(n):
     """ Helper function that compute sum of factors of number n. """
-    results = set()
-    if n == 1:
-        return 1
-    # 从1到n/2，逐个寻找因子
-    for i in range(1, int(math.sqrt(n/2))):
+    results = {1, n}
+    # 从1到n ** 0.5，逐个寻找因子
+    for i in range(2, int(math.sqrt(n)+1)):
         if n % i == 0:
             results.add(i)
             results.add(int(n/i))
@@ -28,4 +26,4 @@ def sum_of_factors(n):
 
 
 if __name__ == '__main__':
-    print(list_squared(42, 5000))
+    print(list_squared(1, 250))
