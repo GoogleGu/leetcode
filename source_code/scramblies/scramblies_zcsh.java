5d3a7afd063a9100439ea2ea3e180e9341d05b69
Zcsh <43027219+zcsh0721@users.noreply.github.com>
2019-01-15 17:30:41 +0800

    Update scramblies_zcsh.java

--- source_code/scramblies/scramblies_zcsh.java
+++ source_code/scramblies/scramblies_zcsh.java
@@ -12,17 +12,17 @@ public class scramblies_zcsh {
 
         int[] counter = new int[26];
 
-        char[] chars = str1.toCharArray();
+        char[] chars = str2.toCharArray();
         for (int i = 0; i < chars.length; i++) {
             counter[chars[i] - 97]++;
         }
-        char[] chars1 = str2.toCharArray();
+        char[] chars1 = str1.toCharArray();
         for (int i = 0; i < chars1.length; i++) {
             counter[chars1[i] - 97]--;
         }
 
-        for (int i = 0; i < chars1.length; i++) {
-            if (chars1[i] > 0) {
+        for (int i = 0; i < counter.length; i++) {
+            if (counter[i] > 0) {
                 return false;
             }
         }
