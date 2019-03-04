b7c9edada9903e32e9fba50813bd08e8f98b7753
Zcsh <43027219+zcsh0721@users.noreply.github.com>
2019-03-04 22:25:00 +0800

    Create SolutionZcsh.java

---
+++ offer/01_search_in_2d_array/SolutionZcsh.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public boolean find(int target, int [][] array) {
+        int y = array.length - 1;
+        int x = 0;
+        int maxX = array[0].length - 1;
+        while (y >= 0 && x <= maxX) {
+            int current = array[y][x];
+            if (target < current) {
+                y--;
+            } else if (target > current) {
+                x++;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
