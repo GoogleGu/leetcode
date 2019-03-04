bdea927756e72d90d272558032b22ef2315e3fe9
Zcsh <43027219+zcsh0721@users.noreply.github.com>
2019-03-04 22:27:02 +0800

    Create SolutionZcsh.java

---
+++ offer/03_print_linked_list/SolutionZcsh.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        
+        if (listNode == null) {
+            return new ArrayList(0);
+        }
+        int size = 0;
+        ListNode beforeNode = null;
+        while (listNode.next != null) {
+            size++;
+            ListNode temp = listNode.next;
+            listNode.next = beforeNode;
+            beforeNode = listNode;
+            listNode = temp;
+        }
+        listNode.next = beforeNode;
+        
+        ArrayList<Integer> result = new ArrayList(size);
+        while (listNode != null) {
+            result.add(listNode.val);
+            listNode = listNode.next;
+        }
+        return result;
+    }
+}
