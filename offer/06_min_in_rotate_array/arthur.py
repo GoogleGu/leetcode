56b9013f0cabb0f0339d42c173758895e9cf7972
arthur_gu <gyx152540@163.com>
2019-03-09 16:42:37 +0800

    第六题改用二分查找

--- offer/06_min_in_rotate_array/arthur.py
+++ offer/06_min_in_rotate_array/arthur.py
@@ -14,3 +14,22 @@ class Solution:
             if current_num < first_num:
                 return current_num
         return first_num
+
+    @staticmethod
+    def min_number_in_rotate_array_through_binary_search(rotate_array):
+        """
+        运行时间：1019ms
+        占用内存：5856k
+        """
+        if not rotate_array:
+            return 0
+
+        left_index = 0
+        right_index = len(rotate_array)-1
+        while left_index < right_index-1:
+            middle_index = (left_index + right_index) // 2
+            if rotate_array[left_index] <= rotate_array[middle_index]:
+                left_index = middle_index
+            else:
+                right_index = middle_index
+        return rotate_array[right_index]
