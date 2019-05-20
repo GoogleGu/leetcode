9dafceb0f5becd2f2ac0ea17f27d686a9c5d0103
JPhoebe <4086824@qq.com>
2019-05-20 17:21:41 +0800

    Update JPhoebe.py

--- offer/24/JPhoebe.py
+++ offer/24/JPhoebe.py
@@ -3,7 +3,9 @@ class Solution:
     def FindPath(self, root, expectNumber):
         current_path = []
         all_path = []
-        return self.find(root, expectNumber, current_path, all_path)
+        all_path = self.find(root, expectNumber, current_path, all_path)
+        return all_path
+        ## return self.shell_sort(all_path)
 
     def find(self, root, expect_number, current_path, all_path):
         if root is None:
@@ -20,3 +22,16 @@ class Solution:
         # 要后退一步，所以要弹出最后最后一位
         current_path.pop()
         return all_path
+
+    def shell_sort(self, value):
+        n = len(value)
+        gap = n//2
+        while gap > 0:
+            for i in range(gap, n):
+                for j in range(i, gap-1, -gap):
+                    if len(value[j]) > len(value[j-gap]):
+                        value[j], value[j-gap] = value[j-gap], value[j]
+                    else:
+                        break
+            gap //= 2
+        return value
