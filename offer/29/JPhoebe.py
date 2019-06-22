d57efbf48049a1d8dc9e4f7387226b8f7241b24c
JPhoebe <4086824@qq.com>
2019-06-22 17:01:20 +0800

    Create JPhoebe.py

---
+++ offer/29/JPhoebe.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def GetLeastNumbers_Solution(self, tinput, k):
+        if k > len(tinput):
+            return []
+        self.quick_sort(tinput, 0, len(tinput)-1)
+        return tinput[:k]
+
+    def quick_sort(self, array, left, right):
+        if left >= right:
+            return
+        low = left
+        high = right
+        key = array[low]  # 第一个值
+
+        while low < high:
+            while low < high and array[high] >= key:
+                high -= 1
+            array[low] = array[high]
+            array[high] = key
+
+            while low < high and array[low] <= key:
+                low += 1
+            array[high] = array[low]
+            array[low] = key
+
+        self.quick_sort(array, left, low - 1)
+        self.quick_sort(array, low + 1, right)
