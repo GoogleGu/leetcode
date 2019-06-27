9c3a7decb4e652a5de4b041ae6f93cf5d57edc6f
arthur_gu <gyx152540@163.com>
2019-06-27 17:55:40 +0800

    上传第28题

--- offer/29/arthur.py
+++ offer/29/arthur.py
@@ -1,5 +1,10 @@
+import pysnooper
+
 
 class Solution:
+
+
+    @pysnooper.snoop()
     def GetLeastNumbers_Solution(self, tinput, k):
         if k > len(tinput) or k == 0:
             return []
@@ -14,14 +19,19 @@ class Solution:
             index = partition(tinput, start, end)
         return tinput[:k]
 
-
+@pysnooper.snoop()
 def partition(tinput, start, end):
-        j = start - 1
+        j = start
         cmp = tinput[end]
         for i in range(start, end):
             if tinput[i] < cmp:
-                j += 1
                 tinput[i], tinput[j] = tinput[j], tinput[i]
-        j += 1
+                j += 1
         tinput[end], tinput[j] = tinput[j], tinput[end]
         return j
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    input = [9,4,7,8,3,6,43]
+    solution.GetLeastNumbers_Solution(input, k=4)
