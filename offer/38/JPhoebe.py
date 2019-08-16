ab05790dfde5203aa7f42ea3fb5c18b3ec697dfe
JPhoebe <4086824@qq.com>
2019-08-16 10:26:54 +0800

    Create JPhoebe.py

---
+++ offer/38/JPhoebe.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    def TreeDepth(self, pRoot):
+        if not pRoot:
+            return 0
+        else:
+            left = self.TreeDepth(pRoot.left)
+            right = self.TreeDepth(pRoot.right)
+            return 1 + left if left > right else 1 + right
