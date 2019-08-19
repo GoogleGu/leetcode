5c995f687fb9760359abff65848d945cda707dce
keben <33311918+2366597634@users.noreply.github.com>
2019-08-19 16:47:00 +0800

    Update Keben.java

--- offer/38/Keben.java
+++ offer/38/Keben.java
@@ -1,20 +1,16 @@
 public class TreeDepth {
 
-
-    public static boolean TreeDepth(TreeNode root) {
-        return depth(root, 0) != 8888888;
+    public int TreeDepth(TreeNode root) {
+        return depth(root, 0);
     }
 
-    public static int depth(TreeNode node, int index) {
+    public int depth(TreeNode node, int index) {
         if (node == null) {
             return index;
         }
         index++;
         int leftIndex = depth(node.left, index);
         int rightIndex = depth(node.right, index);
-        if (leftIndex - rightIndex > 1 || rightIndex - leftIndex > 1) {
-            return 8888888;
-        }
         return Math.max(leftIndex, rightIndex);
     }
 
