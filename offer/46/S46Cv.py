294d4f449d29f7dae90fa194162eb9dcf0a04b6b
cv <cvzmcc@163.com>
2019-09-16 19:03:33 +0800

    c.v.

--- offer/46/S46Cv.py
+++ offer/46/S46Cv.py
@@ -21,8 +21,8 @@ class Solution:
         if n < 1 or m < 1:
             return -1
         last = 0
-        for i in range(2, n):
-            last = (last + m) % n
+        for i in range(2, n+1):
+            last = (last + m) % i
         return last
 
 
