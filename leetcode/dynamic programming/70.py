89b938de6d5b29b79ef70b0219c61d1e80dbb0f6
Yanxuan Gu <gyx152540@gmail.com>
2020-03-10 10:49:22 +0800

    完成leetcode #70

---
+++ leetcode/dynamic programming/70.py
@@ -0,0 +1,39 @@
+"""
+爬楼梯
+
+链接：https://leetcode-cn.com/problems/climbing-stairs
+
+假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+
+每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+
+注意：给定 n 是一个正整数。
+
+示例 1：
+
+输入： 2
+输出： 2
+解释： 有两种方法可以爬到楼顶。
+1.  1 阶 + 1 阶
+2.  2 阶
+示例 2：
+
+输入： 3
+输出： 3
+解释： 有三种方法可以爬到楼顶。
+1.  1 阶 + 1 阶 + 1 阶
+2.  1 阶 + 2 阶
+3.  2 阶 + 1 阶
+"""
+
+class Solution:
+
+
+    def climbStairs(self, n):
+        ways = {1: 1, 2: 2}
+        if n < 3:
+            return ways[n]
+        for i in range(3, n+1):
+            ways[i] = ways[i-1] + ways[i-2]
+        return ways[n]
+
